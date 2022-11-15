a8b5c2d8fc596ed6b0212b1a4add00a81f2b38a0
omarmoustafa11 <omaroj99@yahoo.com>
2022-11-15 18:07:55 -0500

    controller

--- src/main/java/com/BankingAPI/BankingAPIDEMO/customer/CustomerService.java
+++ src/main/java/com/BankingAPI/BankingAPIDEMO/customer/CustomerService.java
@@ -2,6 +2,8 @@ package com.BankingAPI.BankingAPIDEMO.customer;
 
 import com.BankingAPI.BankingAPIDEMO.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,38 +14,27 @@ import java.util.Optional;
 public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
-    @Autowired
-    AccountService accountService;
 
-    public Customer createCustomer(Customer customer){
-        return customerRepository.save(customer);
-    }
-
-    public List<Customer> getAllCustomers() {
-        List<Customer> listOfCustomers = new ArrayList<>();
-        customerRepository.findAll().forEach(listOfCustomers::add);
-        return listOfCustomers;
-    }
-
-    public Optional<Customer> getCustomerByAccountId(Long account_id) {
-        Long customerId = accountService.getAccountByAccountId(account_id).get().getCustomerId();
-        return customerRepository.findById(customerId);
-    }
-
-    public Optional<Customer> getCustomerByCustomerId(Long id) {
-        return customerRepository.findById(id);
-    }
-
-    public boolean customerCheck(Long customerId){
-        Customer customer = customerRepository.findById(customerId).orElse(null);
-        return customer != null;
-    }
-
-    public void updateCustomer(Customer customer) {
+    public void createCustomer(Customer customer) {
         customerRepository.save(customer);
     }
 
-//    public void deleteCustomer(Long id) {
-//        customerRepository.deleteById(id);
-//    }
+    public ResponseEntity<Iterable<Customer>> getAllCustomers() {
+        Iterable<Customer> allCustomers = customerRepository.findAll();
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> getCustomerById(Long customerId) {
+        Optional<Customer> c = customerRepository.findById(customerId);
+        return new ResponseEntity<>(c, HttpStatus.OK);
+    }
+
+    public void updateCustomer(Customer customer, Long Id) {
+        customerRepository.save(customer);
+    }
+
+    public ResponseEntity<?> getCustomerByAccountId (Long accountId){
+        Optional<Customer> c = customerRepository.findById(accountId);
+        return new ResponseEntity<>(c,HttpStatus.OK);
+    }
 }
