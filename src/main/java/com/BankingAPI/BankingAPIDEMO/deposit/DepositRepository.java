0358a98166d377056a5251eed7f95632a9d3170a
Aqilalh <Aqilalhaidari@gmail.com>
2022-11-15 15:16:39 -0500

    doing deposit

--- src/main/java/com/BankingAPI/BankingAPIDEMO/deposit/DepositRepository.java
+++ src/main/java/com/BankingAPI/BankingAPIDEMO/deposit/DepositRepository.java
@@ -1,4 +1,9 @@
 package com.BankingAPI.BankingAPIDEMO.deposit;
 
-public interface DepositRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepositRepository extends CrudRepository<Deposit,Long> {
+    Iterable<Deposit> findDepositByAccountId(Long accountId);
 }
