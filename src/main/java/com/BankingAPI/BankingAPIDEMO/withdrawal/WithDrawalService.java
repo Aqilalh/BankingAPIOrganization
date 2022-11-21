378edc57aeaefeaec23c1e3289bf9f9268e91c87
omarmoustafa11 <omaroj99@yahoo.com>
2022-11-21 12:27:12 -0500

    Done withdrawl

--- src/main/java/com/BankingAPI/BankingAPIDEMO/withdrawal/WithDrawalService.java
+++ src/main/java/com/BankingAPI/BankingAPIDEMO/withdrawal/WithDrawalService.java
@@ -3,6 +3,7 @@ package com.BankingAPI.BankingAPIDEMO.withdrawal;
 
 
 import com.BankingAPI.BankingAPIDEMO.account.Account;
+import com.BankingAPI.BankingAPIDEMO.account.AccountRepository;
 import com.BankingAPI.BankingAPIDEMO.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,8 +16,8 @@ import java.util.logging.Logger;
 @Service
 public class WithDrawalService {
 
-    Logger withdrawalLog = LoggerFactory.getLogger(WithDrawalController.class);
-
+    @Autowired
+    private AccountRepository accountRepository;
     @Autowired
     private WithdrawalRepository withdrawalRepository;
 
@@ -26,13 +27,15 @@ public class WithDrawalService {
     public Iterable<WithDrawal> getAllWithdrawalById(Long accountId) {
         return withdrawalRepository.getWithdrawalByAccountId(accountId);
     }
+    public Optional<WithDrawal> getWithdrawalByWithdrawalId(Long withdrawalId) {
+
+        return withdrawalRepository.findById(withdrawalId);
+    }
 
 
     public WithDrawal createWithdrawal(WithDrawal withdrawal, Long accountId) {
 
-        withdrawalLog.info("===== CREATING WITHDRAWAL =====");
         Optional<Account> account = accountService.getAccountByAccountId(accountId);
-
         Double accountBalance = account.get().getBalance();
         Double withdrawalAmount = withdrawal.getAmount();
 
@@ -42,12 +45,43 @@ public class WithDrawalService {
         return withdrawalRepository.save(withdrawal);
     }
     public void updateWithdrawal(WithDrawal withDrawal, Long accountId) {
+
+        Account account = accountService.getAccountByAccountId(withDrawal.getPayer_id()).orElse(null);
+
+        Double oldWithdrawalAmount = withdrawalRepository.findById(accountId).get().getAmount();
+
+        Double accountBalance = account.getBalance();
+
+        Double oldBalance = accountBalance + oldWithdrawalAmount;
+        account.setBalance(oldBalance);
+
+        Double depositAmount = withDrawal.getAmount();
+
+        Double transaction = oldBalance - depositAmount;
+        account.setBalance(transaction);
+
         withdrawalRepository.save(withDrawal);
     }
+
 
     public void deleteWithdrawals( Long WithdrawalId) {
         withdrawalRepository.deleteById(WithdrawalId);
     }
+
+
+    public boolean accountCheck(Long accountId) {
+            Account account = accountRepository.findById(accountId).orElse(null);
+            return account != null;
+    }
+
+    public boolean withdrawalCheck(Long withdrawalId) {
+        WithDrawal withDrawal = withdrawalRepository.findById(withdrawalId).orElse(null);
+        return withDrawal != null;
+    }
+    public void deleteWithdrawal(Long withdrawalId) {
+        withdrawalRepository.deleteById(withdrawalId);
+    }
+
 
 
 }
